79425f0f3ab1dcbd662f53aa15bd41543cf45a35
David Acosta <davidacosta25081@yahoo.com>
2019-03-18 23:20:29 +0000

    Creating Scraper class

---
+++ lib/best_books_of_2018/scraper.rb
@@ -0,0 +1,17 @@
+class BestBooksOf2018::Scraper
+
+
+  def  get_page
+    Nokogiri::HTML(open("https://www.washingtonpost.com/graphics/2018/entertainment/books/best-books-of-2018/?noredirect=on&utm_term=.6d3fce89dfc"))
+  end
+
+  def scrape_books
+    self.get_page.css("div.book")
+  end
+
+  def books_maker
+    scrape_books.each { |b|
+      BestBooksOf2018::Book.new_book(b) }
+  end
+
+end
