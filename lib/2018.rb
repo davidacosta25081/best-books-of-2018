d98f47638f74c8a264ec0be4e0ba5f70cd4cf951
David Acosta <davidacosta25081@yahoo.com>
2019-03-18 21:58:14 +0000

    first commit

---
+++ lib/2018.rb
@@ -0,0 +1,12 @@
+require "best/books/of/2018/version"
+
+module Best
+  module Books
+    module Of
+      module 2018
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/best/books/of/2018/version.rb
@@ -0,0 +1,9 @@
+module Best
+  module Books
+    module Of
+      module 2018
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
