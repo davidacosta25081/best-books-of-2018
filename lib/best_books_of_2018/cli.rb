e63e2d39b1d077ea2dea7602c72bf36ddd39af6e
David Acosta <davidacosta25081@yahoo.com>
2019-03-19 00:38:06 +0000

    creating CLI menus

--- lib/best_books_of_2018/cli.rb
+++ lib/best_books_of_2018/cli.rb
@@ -1,1 +1,10 @@
-class BestBooksof2018:: CLI
+class BestBooksof2018::CLI
+
+def call
+  BestBooksOf2018::Scraper.new.books_maker
+  list_books
+  menu
+end
+
+def list_books
+  puts "Welcome to The Washington Post Best Books of 2018 list"  
